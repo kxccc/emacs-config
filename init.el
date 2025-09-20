a7ab8a624dd8746c922dc9b101930912031f6495
czc <zckxccc@gmail.com>
2025-09-20 22:31:30 +0800

    1. 模块化 2. 设置主题 3. 增加org设置

--- init.el
+++ init.el
@@ -8,36 +8,21 @@
 
 ;; 自动安装未安装的包
 (setq use-package-always-ensure t)
-
 ;; 启用 use-package
 (eval-when-compile
   (require 'use-package))
 
-;; evil
-(use-package evil
-  :init
-  (setq evil-want-C-u-scroll t)
-  (setq evil-undo-system 'undo-redo)
-  :config
-  (evil-mode 1)
-  (define-key evil-normal-state-map (kbd "SPC") 'evil-window-map))
-
-;; evil-org
-(use-package evil-org
-  :after (evil org)
-  :hook (org-mode . evil-org-mode)
-  :config
-  (require 'evil-org-agenda)
-  (evil-org-agenda-set-keys))
-
 (electric-pair-mode 1)            ; 自动补全括号
 (column-number-mode 1)            ; 在 Mode line 上显示列号
 (global-display-line-numbers-mode 1)     ; 在 Window 显示行号
-(which-key-mode 1) ; which-key
-(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font Mono-16") ; 设置字体
+(global-auto-revert-mode t)              ; 当另一程序修改了文件时，让 Emacs 及时刷新 Buffer
+(which-key-mode 1)                       ; which-key
+(set-face-attribute 'default nil :font "Maple Mono Normal NF CN-16")    ; 设置字体
 
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory)) ; 设定源码加载路径
-(require 'init-org)
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory)) ; 添加自定义配置文件路径
+(require 'init-evil)          ; 设置 evil
+(require 'init-org)           ; 设置 org-mode
+(require 'init-doom-themes)   ; 设置 doom-themes
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,9 +1,30 @@
 ;;; init-org
-;;; Commentary:
-;;; Code:
 
-(defun hello-world ()
- (interactive)
- (message "Hello, world!"))
+;; 打开 agenda 快捷键
+(evil-define-key 'normal 'global (kbd "<leader>a") 'org-agenda)
+
+;; org 目录
+(setq org-directory "~/dev/docs/obsidian")
+;; org agenda 读取目录
+(setq org-agenda-files (directory-files-recursively org-directory "\\.org$"))
+;; 设置启动默认目录
+(setq default-directory org-directory)
+
+;; 开启 desktop 保存功能
+(desktop-save-mode 1)
+;; 每 10 分钟自动保存
+(setq desktop-auto-save-timeout 600)
+
+;; 避免每次执行代码都确认
+(setq org-confirm-babel-evaluate nil)
+;; 启用 Org Babel 支持 Python
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((python . t)))
+;; 指定使用 Python 3
+(setq org-babel-python-command "python3")
+
+;; 记录任务完成时间
+(setq org-log-done 'time)
 
 (provide 'init-org)
